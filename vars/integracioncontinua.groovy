07a7e527e2c90a031496a52fd65f7e373140c807
duribef <diego.uribe.f@gmail.com>
2022-02-04 12:13:51 -0300

    ahora si pom version como variable

--- vars/integracioncontinua.groovy
+++ vars/integracioncontinua.groovy
@@ -122,11 +122,11 @@ def call(Map args) {
                                     packages: [[$class: 'MavenPackage',
                                                 mavenAssetList: [[classifier: '',
                                                                 extension: '',
-                                                                filePath: 'build/DevOpsUsach2020-0.0.1.jar']],
+                                                                filePath: "build/DevOpsUsach2020-${mavenPom.version}.jar"]],
                                                 mavenCoordinate: [artifactId: 'DevOpsUsach2020',
                                                                 groupId: 'com.devopsusach2020',
                                                                 packaging: 'jar',
-                                                                version: '0.0.1']]]
+                                                                version: "${mavenPom.version}"]]]
                 }
             }
         //    stage('Create Release') {
