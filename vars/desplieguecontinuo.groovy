f7024390bb6b89827f6fcc5441289767be3e2bc5
duribef <diego.uribe.f@gmail.com>
2022-02-03 21:53:49 -0300

    sdsd

--- vars/desplieguecontinuo.groovy
+++ vars/desplieguecontinuo.groovy
@@ -10,39 +10,39 @@ def call(Map pipelineParameters) {
             STAGE = ' '
         }
         stages {
-            // stage('gitDiff') {
-            //     //- Mostrar por pantalla las diferencias entre la rama release en curso y la rama
-            //     //master.(Opcional)
-            //     steps {
-            //         script { STAGE = 'gitDiff ' }
-            //         sh 'echo gitDiff'
-            //         sh 'git diff release/release-v1-0-0 origin/main'
-            //     }
-            // }
-            // stage('nexusDownload') {
-            //     //- Descargar el artefacto creado al workspace de la ejecución del pipeline.
-            //     steps {
-            //         script { STAGE = 'nexusDownload ' }
-            //         sh 'sleep 5 '
-            //         sh 'echo nexusDownload'
-            //         sh 'curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/$VERSION/DevOpsUsach2020-$VERSION.jar -O'
-            //     }
-            // }
-            // stage('Run Jar') {
-            //     //- Ejecutar artefacto descargado.
-            //     steps {
-            //         script { STAGE = 'Run Jar ' }
-            //         sh 'echo Run Jar'
-            //         sh 'nohup java -jar DevOpsUsach2020-$VERSION.jar & >/dev/null'
-            //     }
-            // }
+            stage('gitDiff') {
+                //- Mostrar por pantalla las diferencias entre la rama release en curso y la rama
+                //master.(Opcional)
+                steps {
+                    script { STAGE = 'gitDiff ' }
+                    sh 'echo gitDiff'
+                    sh 'git diff release/release-v1-0-0 origin/main'
+                }
+            }
+            stage('nexusDownload') {
+                //- Descargar el artefacto creado al workspace de la ejecución del pipeline.
+                steps {
+                    script { STAGE = 'nexusDownload ' }
+                    sh 'sleep 5 '
+                    sh 'echo nexusDownload'
+                    sh 'curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/$VERSION/DevOpsUsach2020-$VERSION.jar -O'
+                }
+            }
+            stage('Run Jar') {
+                //- Ejecutar artefacto descargado.
+                steps {
+                    script { STAGE = 'Run Jar ' }
+                    sh 'echo Run Jar'
+                    sh 'nohup java -jar DevOpsUsach2020-$VERSION.jar & >/dev/null'
+                }
+            }
             stage('test') {
                 //- Realizar llamado a microservicio expuesto en local para cada uno de sus
                 //métodos y mostrar los resultados.
                 steps {
                     script { STAGE = 'test ' }
                     sh 'echo Test Curl'
-                // sh "sleep 30 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+                    sh "sleep 30 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
                 }
                 post {
                     success {
--- vars/integracioncontinua.groovy
+++ vars/integracioncontinua.groovy
@@ -37,7 +37,7 @@ def call(Map pipelineParameters) {
                     script { STAGE = 'Compile ' }
                     sh "echo 'Compile Code!'"
                     // Run Maven on a Unix agent.
-                    // sh 'mvn clean compile -e'
+                    sh 'mvn clean compile -e'
                 }
             }
             stage('Unit Test') {
@@ -46,7 +46,7 @@ def call(Map pipelineParameters) {
                     script { STAGE = 'Unit Test ' }
                     sh "echo 'Test Code!'"
                     // Run Maven on a Unix agent.
-                    // sh 'mvn clean test -e'
+                    sh 'mvn clean test -e'
                 }
             }
             stage('Build jar') {
@@ -55,7 +55,7 @@ def call(Map pipelineParameters) {
                     script { STAGE = 'Build jar ' }
                     sh "echo 'Build .Jar!'"
                     // Run Maven on a Unix agent.
-                    // sh 'mvn clean package -e'
+                    sh 'mvn clean package -e'
                 }
             }
             stage('SonarQube') {
@@ -67,10 +67,10 @@ def call(Map pipelineParameters) {
                 steps {
                     script { STAGE = 'SonarQube ' }
                     sh "echo 'SonarQube'"
-                    // withSonarQubeEnv('sonarqube') {
-                    //     sh "echo 'SonarQube'"
-                    //     sh 'mvn clean verify sonar:sonar -Dsonar.projectKey=covid-devops'
-                    // }
+                    withSonarQubeEnv('sonarqube') {
+                        sh "echo 'SonarQube'"
+                        sh 'mvn clean verify sonar:sonar -Dsonar.projectKey=covid-devops'
+                    }
                 }
                 post {
                     //- Subir el artefacto creado al repositorio privado de Nexus.
@@ -78,37 +78,37 @@ def call(Map pipelineParameters) {
                     success {
                         script { STAGE = 'Subir a Nexus ' }
                         sh "echo 'Subir a nexus'"
-                        // nexusPublisher nexusInstanceId: 'nexus',
-                        //                      nexusRepositoryId: 'devops-usach-nexus',
-                        //                     packages: [[$class: 'MavenPackage',
-                        //                          mavenAssetList: [[classifier: '',
-                        //                                          extension: '',
-                        //                                          filePath: 'build/DevOpsUsach2020-0.0.1.jar']],
-                        //                          mavenCoordinate: [artifactId: 'DevOpsUsach2020',
-                        //                                          groupId: 'com.devopsusach2020',
-                        //                                          packaging: 'jar',
-                        //                                          version: VERSION]]]
+                        nexusPublisher nexusInstanceId: 'nexus',
+                                             nexusRepositoryId: 'devops-usach-nexus',
+                                            packages: [[$class: 'MavenPackage',
+                                                 mavenAssetList: [[classifier: '',
+                                                                 extension: '',
+                                                                 filePath: 'build/DevOpsUsach2020-0.0.1.jar']],
+                                                 mavenCoordinate: [artifactId: 'DevOpsUsach2020',
+                                                                 groupId: 'com.devopsusach2020',
+                                                                 packaging: 'jar',
+                                                                 version: VERSION]]]
                     }
                 }
             }
-        //        stage('Create Release') {
-        //            //- Crear rama release cuando todos los stages anteriores estén correctamente ejecutados.
-        //            //- Este stage sólo debe estar disponible para la rama develop.
-        //            when {
-        //                branch 'develop'
-        //            }
-        //            steps {
-        //                script { STAGE = 'Create Release ' }
-        //                sh "echo 'gitCreateRelease'"
-        //                withCredentials([gitUsernamePassword(credentialsId: 'github-token')]) {
-        //                    sh '''
-        //                        git checkout -b release/release-v$FINAL_VERSION
-        //                        git push origin release/release-v$FINAL_VERSION
-        //                       '''
-        //                }
-        //            //solo cuando es develop debo crear rama release.
-        //            }
-        //        }
+            //    stage('Create Release') {
+            //        //- Crear rama release cuando todos los stages anteriores estén correctamente ejecutados.
+            //        //- Este stage sólo debe estar disponible para la rama develop.
+            //        when {
+            //            branch 'develop'
+            //        }
+            //        steps {
+            //            script { STAGE = 'Create Release ' }
+            //            sh "echo 'gitCreateRelease'"
+            //            withCredentials([gitUsernamePassword(credentialsId: 'github-token')]) {
+            //                sh '''
+            //                    git checkout -b release/release-v$FINAL_VERSION
+            //                    git push origin release/release-v$FINAL_VERSION
+            //                   '''
+            //            }
+            //        //solo cuando es develop debo crear rama release.
+            //        }
+            //    }
         }
 
         post {
